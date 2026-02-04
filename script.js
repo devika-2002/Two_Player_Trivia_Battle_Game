01f3a86d90a69b5e7fd52dbb3cbcd8e6fc91b0eb
devika-2002 <devika21@navgurukul.org>
2026-02-05 02:12:52 +0530

    working.

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const PlayerSetupSection = document.getElementById("player-setup-section");
 const categorySelectionSection = document.getElementById("category-selection-section");
+const questionGameplaySection = document.getElementById("question-gameplay-section");
 
 const startGameButton = document.getElementById("start-game-button");
 const startRoundButton = document.getElementById("start-round-button");
 
+let index = 0;
+
 let player1Name = "";
 let player2Name = "";
 
+let currentCategory = "";
+const difficultyList = ["easy", "medium", "hard"];
+let currentDifficulty = "";
+
 categorySelectionSection.style.display = "none";
+questionGameplaySection.style.display = "none";
 
 startGameButton.addEventListener("click", function() {
     const player1Input = document.getElementById("player1");
     const player2Input = document.getElementById("player2");
     
-    const player1Name = player1Input.value;
-    const player2Name = player2Input.value;
+    player1Name = player1Input.value;
+    player2Name = player2Input.value;
     
     const error = document.getElementById("error-msg");
     error.textContent = "";
@@ -42,18 +50,47 @@ startGameButton.addEventListener("click", function() {
 startRoundButton.addEventListener("click", function () {
     const categoryDropdown = document.getElementById("category-dropdown");
     const selectedCategory = categoryDropdown.value;
-
-    const difficulty = ["easy", "medium", "hard"];
+    
+    currentCategory = selectedCategory;
 
     categoryDropdown.remove(categoryDropdown.selectedIndex);
+    
+    categorySelectionSection.style.display = "none";
+    questionGameplaySection.style.display = "block";
+    
+    const roundNumberElement = document.getElementById("round-number");
+    roundNumberElement.textContent = "Round: " + (index + 1);
+    
+    const categoryName = document.getElementById("category-name");
+    categoryName.textContent = "Category: " + currentCategory;
+    
+    let currentDifficulty = "";
 
-    fetchQuestions(selectedCategory, difficulty);
+    if (index < 2) {
+      currentDifficulty = difficultyList[0]; 
+    } else if (index < 4) {
+      currentDifficulty = difficultyList[1]; 
+    } else {
+      currentDifficulty = difficultyList[2];
+    }
+
+    const difficultyLevel = document.getElementById("difficulty-level");
+    difficultyLevel.textContent = "Difficulty: " + currentDifficulty;
+    
+    const currentPlayerTurn = document.getElementById("current-player-turn");
+    if (index % 2 === 0) {
+        currentPlayerTurn.textContent = "Player Turn: " + player1Name;
+    } else {
+        currentPlayerTurn.textContent = "Player Turn: " + player2Name;
+    }
+   
+    fetchQuestions(selectedCategory, difficultyList);
 });
 
-async function fetchQuestions(category, difficulty) {
+async function fetchQuestions(category, difficultyList) {
     try {
     const response = await fetch(
-      `https://the-trivia-api.com/v2/questions?categories=${category}&difficulties=${difficulty}&limit=2`
+      `https://the-trivia-api.com/v2/questions?categories=${category}&difficulties=${difficultyList}&limit=2`
     );
 
     if (!response.ok) {
@@ -67,4 +104,46 @@ async function fetchQuestions(category, difficulty) {
     console.error("Error:", error);
     }
 }
+
+function showQuestion() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
